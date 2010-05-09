969d855cbd40d5233d72fb0f7e973ee72347d5fd
tokland <tokland@a44083ce-e418-11de-92aa-3ddf338522b3>
2010-05-09 08:56:47 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -1,42 +1,31 @@
 #!/usr/bin/python
-"""Upload videos to Youtube (spliting them if necessary)"""
+"""Upload videos to Youtube with automatic split."""
 
-# Copyright (c) 2008-2009 Arnau Sanchez <tokland@gmail.com>
-
-# This file is part of Youtube-upload.
-
-# This script is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this software.  If not, see <http://www.gnu.org/licenses/>
-
+from youtube_upload import VERSION
 from distutils.core import setup
+import platform
 
-setup(
+setup_kwargs = dict(
     name="youtube-upload",
-    description="Upload videos to Youtube (spliting them if necessary)",
+    version=VERSION,
+    description="Upload videos to Youtube with automatic split",
     author="Arnau Sanchez",
     author_email="tokland@gmail.com",
-    url="http://code.google.com/p/youtube-upload",
+    url="http://code.google.com/p/youtube-upload/",
     packages=[  
-        "youtube_upload",
+        "youtube_upload/",
     ],
     scripts=[
-		    "bin/youtube-upload",
+      "bin/youtube-upload",
 	  ],
     license="GNU Public License v3.0",
     long_description=" ".join(__doc__.strip().splitlines()),
-    data_files = [],
-    classifiers = [
-        'Development Status :: 3 - Alpha',
+#    data_files=[
+#        ('share/pysheng',
+#            ('pysheng/main.glade',)),
+#    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
         'Intended Audience :: End Users/Desktop',
         'License :: OSI Approved :: GNU General Public License (GPL)',
         'Natural Language :: English',
@@ -46,3 +35,5 @@ setup(
         'Topic :: Internet :: WWW/HTTP',
     ],
 )
+
+setup(**setup_kwargs)
