f3f7a4ca9b28dd84e454f912e8f07dc56ae0f863
tokland <tokland@a44083ce-e418-11de-92aa-3ddf338522b3>
2010-06-29 11:46:25 +0000

    fix example

--- examples/upload_with_curl.sh
+++ examples/upload_with_curl.sh
@@ -6,13 +6,19 @@
 
 set -e
 
+debug() { echo "$@" >&2; }
+
 get_video_id_from_headers() {
   grep -m1 "^Location: " | grep -o "id=[^&]\+" | cut -d"=" -f2-
 }
 
 while IFS="|" read FILE TOKEN POST_URL; do
+  test "$FILE" -a "$TOKEN" -a "$POST_URL" || {
+    debug "Wrong input"
+    continue;
+  }    
   REDIRECT_URL="http://code.google.com/p/youtube-upload"
-  VIDEO_ID=$(curl --include -F "token=$TOKEN" -F "file=@$FILE"
+  VIDEO_ID=$(curl --include -F "token=$TOKEN" -F "file=@$FILE" \
              "$POST_URL?nexturl=$REDIRECT_URL" | get_video_id_from_headers)
   echo "http://www.youtube.com/watch?v=$VIDEO_ID"
 done
--- youtube_upload/youtube_upload.py
+++ youtube_upload/youtube_upload.py
@@ -209,7 +209,7 @@ def main_upload(arguments):
         return 1
     
     email, password0, video_path, title, description, category, skeywords = args
-    password = (sys.stdin.read().strip() if password0 == "-" else password0)
+    password = (sys.stdin.readline().strip() if password0 == "-" else password0)
     videos = ([video_path] if options.no_split else list(split_youtube_video(video_path)))
     debug("connecting to Youtube API")
     yt = Youtube(DEVELOPER_KEY, email, password)
