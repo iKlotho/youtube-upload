b26b40dcd322f7f485cb4a2ae0fbaa2b0eac94b4
tokland <tokland@a44083ce-e418-11de-92aa-3ddf338522b3>
2011-04-10 08:31:13 +0000

    fix get video duration

--- examples/split_video_for_youtube.sh
+++ examples/split_video_for_youtube.sh
@@ -2,55 +2,63 @@
 #
 # Split a video file suitable for standard users in Youtube (<15')
 #
-# $ bash split_video_for_youtube.sh video.avi
-# $ youtube-upload video.part*.avi
+#   $ bash split_video_for_youtube.sh video.avi
+#   video.part1.avi
+#   video.part2.avi
 #
-set -e
+#   $ youtube-upload [OPTIONS] video.part*.avi
+#
 
+# Echo to standard error
 debug() { 
   echo "$@" >&2 
 }
 
-# Returns duration (in seconds) of a video (uses ffmpeg).
-# $1: video path
+# Returns duration (in seconds) of a video $1 (uses ffmpeg).
 get_video_duration() {
-  read H M S DS < \
-    <(ffmpeg -i "$1" 2>&1 | 
-      grep -m1 "^[[:space:]]*Duration:" | 
-      cut -d":" -f2- | 
-      cut -d"," -f1 | 
-      sed "s/[:\.]/ /g")
-  echo $((H*3600 + M*60 + S))      
+  local DURATION=$(ffmpeg -i "$1" 2>&1 |
+    grep -m1 "^[[:space:]]*Duration:" |
+    cut -d":" -f2- |
+    cut -d"," -f1 |
+    sed "s/[:\.]/ /g")
+  read HOURS MINUTES SECONDS DECISECONDS <<< "$DURATION"
+  expr $HOURS \* 3600 + $MINUTES \* 60 + $SECONDS      
 }
 
-# Main
-CHUNK_DURATION=900
-FFMPEG_DEFAULT_OPTIONS="-v 1 -sameq"
+main() {
+  set -e
+  CHUNK_DURATION=$((60*15))
+  FFMPEG_DEFAULT_OPTIONS="-v 1 -sameq"
 
-if test $# -eq 0; then
-  debug "Usage: $(basename $0) VIDEO [EXTRA_OPTIONS_FOR_FFMPEG]"
-  exit 1
+  if test $# -eq 0; then
+    debug "Usage: $(basename $0) VIDEO [EXTRA_OPTIONS_FOR_FFMPEG]"
+    exit 1
+  fi
+
+  VIDEO=$1
+  shift 1
+
+  DURATION=$(get_video_duration "$VIDEO")
+
+  if test $DURATION -le $CHUNK_DURATION; then
+    debug "no need to split, duartion $DURATION <= $CHUNK_DURATION"
+    echo "$VIDEO"
+    exit 0
+  fi
+
+  EXTENSION=${VIDEO##*.}
+  BASENAME=$(basename "$VIDEO" ".$EXTENSION")           
+  debug "start split: $VIDEO ($DURATION seconds)"
+  seq 0 $CHUNK_DURATION $DURATION | cat -n | while read INDEX OFFSET; do
+    debug "$VIDEO: from position $OFFSET take $CHUNK_DURATION seconds"
+    OUTPUT_FILE="${BASENAME}.part${INDEX}.avi"
+    # If I use "-vcodec copy" I get unplayable videos, why?  
+    ffmpeg $FFMPEG_DEFAULT_OPTIONS "$@" -i "$VIDEO" -ss $OFFSET -t $CHUNK_DURATION \
+           -y "$OUTPUT_FILE" </dev/null 
+    echo "$OUTPUT_FILE"
+  done
+}
+
+if test "$NOEXEC" != 1; then
+  main "$@"
 fi
-
-VIDEO=$1
-shift 1
-
-EXTENSION=${VIDEO##*.}
-BASENAME=$(basename "$VIDEO" ".$EXTENSION")
-DURATION=$(get_video_duration "$VIDEO")
-
-if test $DURATION -le $CHUNK_DURATION; then
-  debug "no need to split, $DURATION <= $CHUNK_DURATION"
-  echo "$VIDEO"
-  exit 0
-fi
-         
-debug "start split: $VIDEO ($DURATION seconds)"
-seq 0 $CHUNK_DURATION $DURATION | cat -n | while read INDEX OFFSET; do
-  debug "$VIDEO: from position $OFFSET take $CHUNK_DURATION seconds"
-  OUTPUT_FILE="${BASENAME}.part${INDEX}.avi"
-  # using "-vcodec copy" I get unplayable videos, why?  
-  ffmpeg $FFMPEG_DEFAULT_OPTIONS "$@" -i "$VIDEO" -ss $OFFSET -t $CHUNK_DURATION \
-         -y "$OUTPUT_FILE" </dev/null 
-  echo "$OUTPUT_FILE"
-done
