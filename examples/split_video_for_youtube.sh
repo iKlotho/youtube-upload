8f17b72ffd70873c47feb7daa55771550ed34702
tokland <tokland@a44083ce-e418-11de-92aa-3ddf338522b3>
2011-04-10 09:16:22 +0000

    issue39: use mkv + vcodec copy

--- examples/split_video_for_youtube.sh
+++ examples/split_video_for_youtube.sh
@@ -3,10 +3,10 @@
 # Split a video file suitable for standard users in Youtube (<15')
 #
 #   $ bash split_video_for_youtube.sh video.avi
-#   video.part1.avi
-#   video.part2.avi
+#   video.part1.mkv
+#   video.part2.mkv
 #
-#   $ youtube-upload [OPTIONS] video.part*.avi
+#   $ youtube-upload [OPTIONS] video.part*.mkv
 #
 
 # Echo to standard error
@@ -16,25 +16,22 @@ debug() {
 
 # Returns duration (in seconds) of a video $1 (uses ffmpeg).
 get_video_duration() {
-  local DURATION=$(ffmpeg -i "$1" 2>&1 |
-    grep -m1 "^[[:space:]]*Duration:" |
-    cut -d":" -f2- |
-    cut -d"," -f1 |
-    sed "s/[:\.]/ /g")
+  OUTPUT=$(ffmpeg2 -i "$1" -vframes 1 -f rawvideo -y /dev/null 2>&1) ||
+    { debug -e "get_video_duration: error running ffmpeg:\n$OUTPUT"; return 1; }
+  DURATION=$(echo "$OUTPUT" | grep -m1 "^[[:space:]]*Duration:" |
+    cut -d":" -f2- | cut -d"," -f1 | sed "s/[:\.]/ /g") || 
+    { debug -e "get_video_duration: error parsing duration:\n$OUTPUT"; return 1; }
   read HOURS MINUTES SECONDS DECISECONDS <<< "$DURATION"
   expr $HOURS \* 3600 + $MINUTES \* 60 + $SECONDS      
 }
 
 main() {
-  set -e
+  set -e -u -o pipefail
   CHUNK_DURATION=$((60*15))
-  FFMPEG_DEFAULT_OPTIONS="-v 1 -sameq"
-
   if test $# -eq 0; then
     debug "Usage: $(basename $0) VIDEO [EXTRA_OPTIONS_FOR_FFMPEG]"
     exit 1
   fi
-
   VIDEO=$1
   shift 1
 
@@ -48,13 +45,13 @@ main() {
 
   EXTENSION=${VIDEO##*.}
   BASENAME=$(basename "$VIDEO" ".$EXTENSION")           
-  debug "start split: $VIDEO ($DURATION seconds)"
+  debug "start  split: $VIDEO ($DURATION seconds)"
   seq 0 $CHUNK_DURATION $DURATION | cat -n | while read INDEX OFFSET; do
     debug "$VIDEO: from position $OFFSET take $CHUNK_DURATION seconds"
-    OUTPUT_FILE="${BASENAME}.part${INDEX}.avi"
+    OUTPUT_FILE="${BASENAME}.part${INDEX}.mkv"
     # If I use "-vcodec copy" I get unplayable videos, why?  
-    ffmpeg $FFMPEG_DEFAULT_OPTIONS "$@" -i "$VIDEO" -ss $OFFSET -t $CHUNK_DURATION \
-           -y "$OUTPUT_FILE" </dev/null 
+    ffmpeg -v 1 -sameq -vcodec copy -acodec copy "$@" \
+           -i "$VIDEO" -ss $OFFSET -t $CHUNK_DURATION -y "$OUTPUT_FILE" </dev/null 
     echo "$OUTPUT_FILE"
   done
 }
