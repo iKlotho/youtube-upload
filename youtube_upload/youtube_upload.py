dc7291302d1fac7e63aa5f328098868446582c55
tokland <tokland@a44083ce-e418-11de-92aa-3ddf338522b3>
2012-11-06 09:29:12 +0000

    recover deleted lines

--- youtube_upload/youtube_upload.py
+++ youtube_upload/youtube_upload.py
@@ -149,6 +149,8 @@ def post(url, files_params, extra_params, show_progressbar=True):
         bar = None
     body_container = StringIO.StringIO()
     headers_container = StringIO.StringIO()
+    c.setopt(c.WRITEFUNCTION, body_container.write)
+    c.setopt(c.HEADERFUNCTION, headers_container.write)
     c.perform()
     http_code = c.getinfo(pycurl.HTTP_CODE)
     c.close()
